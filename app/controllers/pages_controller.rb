ef9ed5755bf29709643f100a17a1ab4f1551fe38
Sam Lau <samlau95@gmail.com>
2015-02-18 23:56:27 -0800

    Finish step 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @name = params[:name]
   end
 
   def hello
+    name = params[:name]
+    @first_name = name.split.first
+    @last_name = name.split.last
   end
 
   def bye
+    @answer = params[:yes_no]
+    @good = (@answer == "Yup")
   end
 end
