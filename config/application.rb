3f0579e379eb589d22701a51ff4a86e175e4ee75
Sam Lau <samlau95@gmail.com>
2015-02-18 23:33:48 -0800

    Config generators

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Sp15Lecture2
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.helper false
+      generate.javascript_engine false
+      generate.request_specs false
+      generate.controller_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      # generate.test_framework :rspec
+      generate.view_specs false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
